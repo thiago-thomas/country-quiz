f5c93d925601510216237a7befdbb4bfc6926eab
Thiago Thomas <thiago_rocha17@hotmail.com>
2026-02-27 17:59:50 +0000

    feat: restructure header layout and style for country quiz

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ function App() {
   return (
     <>
       <main >
-        <h1>Country Quiz App</h1>
+        <div className="country-quiz__header-container">
+          <h1>Country Quiz</h1>
+          <div className="country-quiz__header-container__points">
+            <span>🏆</span>
+            <span>4/10 Points</span>
+          </div>
+        </div>
       </main>
     </>
   )
