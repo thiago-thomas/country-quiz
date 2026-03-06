d7c7dea52bdc6d828fe860aa6827508af63448fe
Thiago Thomas <thiago_rocha17@hotmail.com>
2026-03-06 23:46:26 +0000

    feat: implementing the initial structure and styling of Congratulations Page

--- src/pages/CongratsPage/index.tsx
+++ src/pages/CongratsPage/index.tsx
@@ -1,8 +1,20 @@
+import CongratsImg from "../../assets/congrats.png";
+import "./style.css";
+
 export function CongratsPage() {
   return (
     <>
-      <div className="country-quiz__main-container">
-        <h2>Congrats!</h2>
+      <div className="country-quiz__main-container congrats-container">
+        <div className="country-quiz__congrats-header">
+          <img src={CongratsImg} alt="Congrats Image" />
+        </div>
+        <div className="country-quiz__congrats-main">
+          <h1>Congrats! You completed the quiz.</h1>
+          <p>You answer 4/10 correctly</p>
+          <button type="button" className="congrats-playAgain">
+            Play again
+          </button>
+        </div>
       </div>
     </>
   );
